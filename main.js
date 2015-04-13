a02cc14f261a4870137bbc390e34be4f61ca3f39
Rado Kirov <radokirov@google.com>
2015-04-13 16:29:18 -0700

    example of an es5 application using angular2 SFX bundle.

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-import {Component, Template, bootstrap} from 'angular2/angular2';
+var Service = function() {};
+Service.prototype.greeting = function() {
+  return 'hello';
+};
 
-@Component({
-  selector: 'cmp',
-  services: [Service]
-})
-@Template({
-  inline: '{{greeting}} world!'
-})
-class Cmp {
-  constructor(service: Service) {
-    this.greeting = service.greeting();
-  }
+var Cmp = function(service) {
+  this.greeting = service.greeting();
 }
+Cmp.annotations = [
+  new angular.Component({
+    selector: 'cmp',
+    injectables: [Service]
+  }),
+  new angular.View({
+    template: '{{greeting}} world!'
+  })
+];
+Cmp.parameters = [[Service]];
 
-class Service {
-  greeting() {
-    return 'hello';
-  }
-}
-
-bootstrap(Cmp);
+document.addEventListener('DOMContentLoaded', function() {
+  angular.bootstrap(Cmp);
+});
