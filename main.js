6e81c5faf1350ef0794776d38c784a1f74eeaa28
Rado Kirov <radokirov@google.com>
2015-05-04 21:57:07 -0700

    update bundle to pre-alpha-21, add routing.

--- main.js
+++ main.js
@@ -3,20 +3,36 @@ Service.prototype.greeting = function() {
   return 'hello';
 };
 
-var Cmp = function(service) {
+var HelloCmp = function(service) {
   this.greeting = service.greeting();
-}
-Cmp.annotations = [
-  new angular.Component({
-    selector: 'cmp',
+};
+HelloCmp.parameters = [[Service]];
+HelloCmp.annotations = [
+  new angular.ComponentAnnotation({
+    selector: 'hello',
     injectables: [Service]
   }),
-  new angular.View({
-    template: '{{greeting}} world!'
+  new angular.ViewAnnotation({
+    template: '<router-outlet></router-outlet>',
+    directives: [angular.router.RouterOutlet]
   })
 ];
-Cmp.parameters = [[Service]];
+
+var Cmp = function() {};
+Cmp.annotations = [
+  new angular.ComponentAnnotation({
+    selector: 'cmp'
+  }),
+  new angular.ViewAnnotation({
+    template: '<router-outlet></router-outlet>',
+    directives: [angular.router.RouterOutlet]
+  }),
+  new angular.router.RouteConfigAnnotation([
+    {path: '/', component: HelloCmp}
+  ])
+];
+
 
 document.addEventListener('DOMContentLoaded', function() {
-  angular.bootstrap(Cmp);
+    angular.bootstrap(Cmp, angular.router.routerInjectables);
 });
